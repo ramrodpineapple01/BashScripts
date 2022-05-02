ca1574e385d634dcd212070912d8cc98e0efa66a
Rick Dawson <dawsora@gmail.com>
2022-05-02 11:52:36 -0400

    Completed conversion to echo_out for verbose

--- setup_desktop.sh
+++ setup_desktop.sh
@@ -98,58 +98,58 @@ echo_out() {
 
 install_airvpn () {
   echo_out "Installing AirVPN client."
-  wget -qO - https://eddie.website/repository/keys/eddie_maintainer_gpg.key | sudo apt-key add - | tee /dev/fd/3
-  echo "deb http://eddie.website/repository/apt stable main" | sudo tee /etc/apt/sources.list.d/eddie.website.list | tee /dev/fd/3
-  sudo apt-get update | tee /dev/fd/3
-  sudo apt-get -y install eddie-ui | tee /dev/fd/3
+  wget -qO - https://eddie.website/repository/keys/eddie_maintainer_gpg.key | sudo apt-key add - | echo_out
+  echo "deb http://eddie.website/repository/apt stable main" | sudo tee /etc/apt/sources.list.d/eddie.website.list | echo_out
+  sudo apt-get update | echo_out
+  sudo apt-get -y install eddie-ui | echo_out
   echo_out "AirVPN Installation Complete"
 }
 
 install_flatpak () {
-  sudo apt-get -y install flatpak
-  sudo apt-get -y install gnome-software-plugin-flatpak
+  sudo apt-get -y install flatpak | echo_out
+  sudo apt-get -y install gnome-software-plugin-flatpak | echo_out
   flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 }
 
 install_ivpn() {
   echo_out "Installing IVPN client."
-  wget -O - https://repo.ivpn.net/stable/ubuntu/generic.gpg | gpg --dearmor > ~/ivpn-archive-keyring.gpg | tee /dev/fd/3
-  sudo mv ~/ivpn-archive-keyring.gpg /usr/share/keyrings/ivpn-archive-keyring.gpg | tee /dev/fd/3
-  wget -O - https://repo.ivpn.net/stable/ubuntu/generic.list | sudo tee /etc/apt/sources.list.d/ivpn.list | tee /dev/fd/3
-  sudo apt update | tee /dev/fd/3
-  sudo apt-get -y install ivpn-ui | tee /dev/fd/3
+  wget -O - https://repo.ivpn.net/stable/ubuntu/generic.gpg | gpg --dearmor > ~/ivpn-archive-keyring.gpg | echo_out
+  sudo mv ~/ivpn-archive-keyring.gpg /usr/share/keyrings/ivpn-archive-keyring.gpg | echo_out
+  wget -O - https://repo.ivpn.net/stable/ubuntu/generic.list | sudo tee /etc/apt/sources.list.d/ivpn.list | echo_out
+  sudo apt update | echo_out
+  sudo apt-get -y install ivpn-ui | echo_out
   echo_out "IVPN Installation Complete"
 }
 
 install_mullvad () {
   echo_out "Installing Mullvad VPN client."
-  wget --content-disposition https://mullvad.net/download/app/deb/latest | tee /dev/fd/3
+  wget --content-disposition https://mullvad.net/download/app/deb/latest | echo_out
   MV_PACKAGE=$(cat ls | grep Mullvad)
-  sudo apt-get -y install ./"${MV_PACKAGE}" | tee /dev/fd/3
+  sudo apt-get -y install ./"${MV_PACKAGE}" | echo_out
   echo_out "Mullvad Installation VPN Complete"
 }
 
 install_nordvpn () {
   echo_out "Installing Nord VPN client."
-  sh <(curl -sSf https://downloads.nordcdn.com/apps/linux/install.sh) | tee /dev/fd/3
-  sudo usermod -aG nordvpn $USER | tee /dev/fd/3
+  sh <(curl -sSf https://downloads.nordcdn.com/apps/linux/install.sh) | echo_out
+  sudo usermod -aG nordvpn $USER | echo_out
   echo_out "Nord VPN Installation Complete"
 }
 install_openvpn () {
   echo_out "Installing OpenVPNclient."
-  sudo curl -fsSL https://swupdate.openvpn.net/repos/openvpn-repo-pkg-key.pub | gpg --dearmor > /etc/apt/trusted.gpg.d/openvpn-repo-pkg-keyring.gpg | tee /dev/fd/3
-  sudo curl -fsSL https://swupdate.openvpn.net/community/openvpn3/repos/openvpn3-$DISTRO.list >/etc/apt/sources.list.d/openvpn3.list | tee /dev/fd/3
-  sudo apt-get update
-  sudo apt-get -y install openvpn3 | tee /dev/fd/3
+  sudo curl -fsSL https://swupdate.openvpn.net/repos/openvpn-repo-pkg-key.pub | gpg --dearmor > /etc/apt/trusted.gpg.d/openvpn-repo-pkg-keyring.gpg | echo_out
+  sudo curl -fsSL https://swupdate.openvpn.net/community/openvpn3/repos/openvpn3-$DISTRO.list >/etc/apt/sources.list.d/openvpn3.list | echo_out
+  sudo apt-get update | echo_out
+  sudo apt-get -y install openvpn3 | echo_out
   echo_out "OpenVPN Installation Complete"
 }
 
 install_protonvpn () {
-  echo_out "Installing ProtonVPNclient."
+  printf "Installing ProtonVPNclient.\n"
   wget https://protonvpn.com/download/protonvpn-stable-release_1.0.1-1_all.deb
-  sudo apt install protonvpn-stable-release_1.0.1-1_all.deb | tee /dev/fd/3
-  sudo apt update | tee /dev/fd/3
-  sudo apt-get -y install protonvpn | tee /dev/fd/3
+  sudo apt install protonvpn-stable-release_1.0.1-1_all.deb | echo_out
+  sudo apt update | echo_out
+  sudo apt-get -y install protonvpn | echo_out
   echo_out "ProtonVPN Installation Complete"
 }
 
@@ -224,7 +224,7 @@ echo_out "\b3" n
 sudo add-apt-repository -y ppa:yubico/stable
 echo_out "\b4"
 sudo add-apt-repository -y ppa:nextcloud-devs/client
-printf "\nComplete\n\n" | tee /dev/fd/3
+printf "Complete\n\n" | tee /dev/fd/3
 
 # Update the base OS
 printf "Updating base OS\n" | tee /dev/fd/3
@@ -232,7 +232,7 @@ sudo apt-get update | echo_out
 sudo apt-get -y install software-properties-common | echo_out
 sudo apt-get -y dist-upgrade | echo_out
 sudo apt-get -y install apt-transport-https | echo_out
-printf "\nComplete\n\n" | tee /dev/fd/3
+printf "Complete\n\n" | tee /dev/fd/3
 
 # Install VirtualBox Guest Additions:
 printf "Installing VirtualBox Guest Additions\n" | tee /dev/fd/3
@@ -243,20 +243,20 @@ sudo apt-get -y install perl | echo_out
 sudo apt-get -y install virtualbox-guest-additions-iso | echo_out
 sudo mount -o loop /usr/share/virtualbox/VBoxGuestAdditions.iso /media/ | echo_out
 /media/autorun.sh
-printf "\nComplete\n\n" | tee /dev/fd/3
+printf "Complete\n\n" | tee /dev/fd/3
 
 # Install Tor Browser:
 printf "Installing TOR browser bundle\n" | tee /dev/fd/3
 ## You may need this if you get a key error
 # gpg --homedir "$HOME/.local/share/torbrowser/gnupg_homedir" --refresh-keys --keyserver keyserver.ubuntu.com
 sudo apt-get -y install torbrowser-launcher | echo_out
-printf "\nComplete\n\n" | tee /dev/fd/3
+printf "Complete\n\n" | tee /dev/fd/3
 
 # GTKHash:
 printf "Installing GTKHash\n" | tee /dev/fd/3
 sudo apt-get install -y gtkhash | echo_out
 #sudo snap install gtkhash | tee /dev/fd/3
-printf "\nComplete\n\n" | tee /dev/fd/3
+printf "Complete\n\n" | tee /dev/fd/3
 
 # Veracrypt:
 printf "Installing Veracrypt\n" | tee /dev/fd/3
@@ -268,7 +268,7 @@ sudo apt-get -y install veracrypt | echo_out
 ## Option 2
 #sudo wget https://launchpad.net/veracrypt/trunk/1.24-update7/+download/veracrypt-console-1.24-Update7-Ubuntu-20.04-amd64.deb
 #sudo apt-get -y install ./veracrypt-console-1.24-Update7-Ubuntu-20.04-amd64.deb | tee /dev/fd/3
-printf "\nComplete\n\n" | tee /dev/fd/3
+printf "Complete\n\n" | tee /dev/fd/3
 
 # Onionshare:
 #TODO: troubleshoot onionshare snap installation
@@ -292,7 +292,7 @@ case ${PACKAGE} in
     sudo snap connect onionshare:removable-media | echo_out
 	;;
 esac
-printf "\nComplete\n\n" | tee /dev/fd/3
+printf "Complete\n\n" | tee /dev/fd/3
 
 # KeepassXC:
 printf "Installing KeePassXC\n" | tee /dev/fd/3
@@ -301,7 +301,7 @@ printf "Installing KeePassXC\n" | tee /dev/fd/3
 sudo snap install keepassxc | echo_out
 sudo snap connect keepassxc:raw-usb | echo_out
 sudo snap connect keepassxc:removable-media | echo_out
-printf "\nComplete\n\n" | tee /dev/fd/3
+printf "Complete\n\n" | tee /dev/fd/3
 
 #Yubikey:
 printf "Installing Yubikey\n" | tee /dev/fd/3
@@ -312,13 +312,13 @@ sudo apt-get -y install libykpers-1-1 | echo_out
 sudo apt-get -y install libpam-u2f | echo_out
 sudo wget https://raw.githubusercontent.com/Yubico/libu2f-host/master/70-u2f.rules -O /etc/udev/rules.d/70-u2f.rules | echo_out
 #sudo mv 70-u2f.rules /etc/udev/rules.d/70-u2f.rules | tee /dev/fd/3
-sudo mkdir -p ~/.config/Yubico | tee /dev/fd/3
-printf "\nComplete\n\n" | tee /dev/fd/3
+sudo mkdir -p ~/.config/Yubico | echo_out
+printf "Complete\n\n" | tee /dev/fd/3
 
 #Nextcloud:
 printf "Installing Nextcloud Client\n\nThis can take a while\n" | tee /dev/fd/3
 sudo apt-get -y install nextcloud-client | echo_out
-printf "\nComplete\n\n" | tee /dev/fd/3
+printf "Complete\n\n" | tee /dev/fd/3
 
 # VPN Clients
 case ${VPN_INSTALL} in
@@ -361,7 +361,7 @@ printf "Cleaning up\n" | tee /dev/fd/3
 sudo apt-get -y autoremove | echo_out
 sudo apt-get -y clean | echo_out
 sudo rm 70-u2f.rules | echo_out # May not exist
-printf "\nComplete\n\n" | tee /dev/fd/3
+printf "Complete\n\n" | tee /dev/fd/3
 
 printf "\n\tPress [Enter] to reboot\n" 1>&3
 read throwaway
