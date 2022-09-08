0a9ac6ca83fa6eb5a625f9834f1b5552f4f1f0f3
Rick Dawson <dawsora@gmail.com>
2022-09-08 10:48:49 -0400

    Clean up kali RDP script

--- setup_kali_rdp.sh
+++ setup_kali_rdp.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 # add RDP to Kali
 # Rick Dawson 2022
+VERSION="1.0.0"
 
 sudo apt update
 sudo apt-get -y dist-upgrade
 
-apt-get install -y kali-desktop-xfce 
-apt-get install -y xorg 
-apt-get install -y xrdp
+sudo apt-get -y install kali-desktop-xfce 
+sudo apt-get -y install xorg 
+sudo apt-get -y install xrdp
 
 sudo systemctl enable xrdp --now
