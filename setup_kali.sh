792895d895b4938a54616456e1f1bc6cedb43739
Rick Dawson <dawsora@gmail.com>
2022-10-04 13:22:06 -0400

    rename kali install log

--- setup_kali.sh
+++ setup_kali.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Kali VM base setup script
 # R. Dawson 2022
-VERSION="2.1.0"
+VERSION="3.0.0"
 
 ## Variables
 # Configuration Variables
@@ -12,67 +12,12 @@ JUP_PATH=/usr/share/jupyter
 #TODO: ADAPTER: This works for a VM, but needs a better method
 ADAPTER1=$(ls /sys/class/net | grep e) 	# 1st Ethernet adapter on VM
 BRANCH="main"							# Default to main branch
-CHECK_IP="8.8.8.8"						# Test ping to google DNS
 DATE_VAR=$(date +'%y%m%d-%H%M')			# Today's Date and time
 LOG_FILE="${DATE_VAR}_install.log"  	# Log File name
 PACKAGE="snap" 							# Install snaps by default
 VPN_INSTALL="false"						# Do not install VPN clients by default
 
 ## Functions
-check_internet() {
-	# SETTINGS
-	TEST="8.8.8.8"       # Test ping to google DNS
-
-	# Report 
-	LOG_FILE=~/Documents/ReportInternet.log
-
-	# Messages
-	MESSAGE1="Attempting to restore connectivity"
-	MESSAGE2="This could take up to 2 minutes"
-	MESSAGE3="No Internet connection detected"
-	MESSAGE4="Internet connection detected"
-
-	# Date
-	TODAY=$(date "+%r %d-%m-%Y")
-
-	# Show IP Public Address
-	IPv4ExternalAddr1=$(ip addr list $ADAPTER1 |grep "inet " |cut -d' ' -f6|cut -d/ -f1)
-	IPv6ExternalAddr1=$(ip addr list $ADAPTER1 |grep "inet6 " |cut -d' ' -f6|cut -d/ -f1)
-
-	# Alarm
-	alarm() {
-		beep -f 1500 -l 200;beep -f 1550 -l 200;beep -f 1500 -l 200;beep -f 1550 -l 200;beep -f 1500 -l 200;beep -f 1550 -l 200;beep -f 1500 -l 200;beep -f 1550$
-	}
-
-	# Restoring Connectivity
-	resolve() {
-		clear
-		echo "$MESSAGE1" | tee /dev/fd/3
-		sudo ifconfig $ADAPTER1 up;sudo dhclient -r $ADAPTER1;sleep 5;sudo dhclient $ADAPTER1
-		echo "$MESSAGE2"
-		sleep 120
-	}
-
-	# Execution of work
-	while true; do
-		if [[ "$(cat /sys/class/net/${ADAPTER1}/operstate)" != "up" ]]; then
-			alarm
-			clear
-			echo "================================================================================" 
-			echo "$MESSAGE3 - $TODAY"                                                               | tee /dev/fd/3
-			echo "================================================================================"
-			sleep 10
-			resolve
-		else
-			clear
-			echo "================================================================================" 
-			echo "$MESSAGE4 - $TODAY - IPv4 Addr: $IPv4ExternalAddr1 - IPv6 Addr: $IPv6ExternalAddr1" | tee /dev/fd/3
-			echo "================================================================================" 
-			break
-		fi
-	done
-}
-
 check_root() {
   # Check to ensure script is not run as root
   if [[ "${UID}" -eq 0 ]]; then
@@ -98,68 +43,6 @@ echo_out() {
   else 
     printf "${MESSAGE}" >> ${LOG_FILE}
   fi
-}
-
-install_airvpn () {
-  printf "Installing AirVPN client.\n" | tee /dev/fd/3
-  wget -qO - https://eddie.website/repository/keys/eddie_maintainer_gpg.key | sudo apt-key add - | echo_out
-  echo "deb http://eddie.website/repository/apt stable main" | sudo tee /etc/apt/sources.list.d/eddie.website.list | echo_out
-  sudo apt-get update | echo_out
-  sudo apt-get -y install eddie-ui | echo_out
-  printf "AirVPN Installation Complete.\n\n" | tee /dev/fd/3
-}
-
-install_flatpak () {
-  printf "Installing Flatpak.\n" | tee /dev/fd/3
-  sudo apt-get -y install flatpak | echo_out
-  sudo apt-get -y install gnome-software-plugin-flatpak | echo_out
-  flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo | echo_out
-  printf "Flatpak Installation Complete.\n\n" | tee /dev/fd/3
-}
-
-install_ivpn() {
-  printf "Installing IVPN client.\n" | tee /dev/fd/3
-  wget -O - https://repo.ivpn.net/stable/ubuntu/generic.gpg | gpg --dearmor > ~/ivpn-archive-keyring.gpg | echo_out
-  sudo mv ~/ivpn-archive-keyring.gpg /usr/share/keyrings/ivpn-archive-keyring.gpg | echo_out
-  wget -O - https://repo.ivpn.net/stable/ubuntu/generic.list | sudo tee /etc/apt/sources.list.d/ivpn.list | echo_out
-  sudo apt update | echo_out
-  sudo apt-get -y install ivpn-ui | echo_out
-  printf "IVPN Installation Complete.\n\n" | tee /dev/fd/3
-}
-
-install_mullvad () {
-  printf "Installing Mullvad VPN client.\n" | tee /dev/fd/3
-  wget --content-disposition https://mullvad.net/download/app/deb/latest | echo_out
-  MV_PACKAGE=$(cat ls | grep Mullvad)
-  sudo apt-get -y install ./"${MV_PACKAGE}" | echo_out
-  rm "${MV_PACKAGE}"
-  printf "Mullvad Installation VPN Complete.\n\n" | tee /dev/fd/3
-}
-
-install_nordvpn () {
-  printf "Installing Nord VPN client.\n" | tee /dev/fd/3
-  sh <(curl -sSf https://downloads.nordcdn.com/apps/linux/install.sh) | echo_out
-  sudo usermod -aG nordvpn $USER | echo_out
-  printf "Nord VPN Installation Complete.\n\n" | tee /dev/fd/3
-}
-
-install_openvpn () {
-  printf "Installing OpenVPNclient.\n" | tee /dev/fd/3
-  sudo curl -fsSL https://swupdate.openvpn.net/repos/openvpn-repo-pkg-key.pub | gpg --dearmor > /etc/apt/trusted.gpg.d/openvpn-repo-pkg-keyring.gpg | echo_out
-  sudo curl -fsSL https://swupdate.openvpn.net/community/openvpn3/repos/openvpn3-$DISTRO.list >/etc/apt/sources.list.d/openvpn3.list | echo_out
-  sudo apt-get update | echo_out
-  sudo apt-get -y install openvpn3 | echo_out
-  printf "OpenVPN Installation Complete.\n\n" | tee /dev/fd/3
-}
-
-install_protonvpn () {
-  printf "Installing ProtonVPNclient.\n" | tee /dev/fd/3
-  wget https://protonvpn.com/download/protonvpn-stable-release_1.0.1-1_all.deb
-  sudo apt install protonvpn-stable-release_1.0.1-1_all.deb | echo_out
-  sudo apt update | echo_out
-  sudo apt-get -y install protonvpn | echo_out
-  rm protonvpn-stable-release_1.0.1-1_all.deb
-  printf "ProtonVPN Installation Complete.\n\n" | tee /dev/fd/3
 }
 
 pause_for () {
@@ -191,12 +74,8 @@ usage() {
 touch ${LOG_FILE}
 
 # Provide usage statement if no parameters
-while getopts cdfhp:v OPTION; do
+while getopts dfhp:v OPTION; do
   case ${OPTION} in
-	c)
-	# Check for internet connection
-	  check_internet "${CHECK_IP}"
-	  ;;
 	d)
 	# Set installation to dev branch
 	  BRANCH="dev"
@@ -215,7 +94,6 @@ while getopts cdfhp:v OPTION; do
 	  VPN_INSTALL="${OPTARG}"
 	  ;;
 	v)
-      # Verbose is first so any other elements will echo as well
       VERBOSE='true'
       echo_out "Verbose mode on."
       ;;
