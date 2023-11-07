75b268ee3f83d876f69ba122ea24bb773cd131b9
Rick Dawson <dawsora@gmail.com>
2023-11-07 13:06:25 -0500

    Update setup_desktop.sh
    Bump Version to 2.8.10

--- setup_desktop.sh
+++ setup_desktop.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Ubuntu VM desktop setup script
 # R. Dawson 2021-2023
-VERSION="2.8.9"
+VERSION="2.8.10"
 
 ## Variables
 #TODO: ADAPTER: This works for a VM, but needs a better method
